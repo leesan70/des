77855f4aa39513b3e1b6c27c35a9e3147e11cf38
MinSoo Kim <minsoo8449@gmail.com>
2017-11-05 17:47:11 -0500

    done

---
+++ android/app/src/main/java/com/sblee/des/util/Utils.java
@@ -0,0 +1,16 @@
+package com.sblee.des.util;
+
+import android.content.Context;
+
+/**
+ * Created by minsoo on 2017. 11. 5..
+ */
+
+public class Utils {
+
+    public static final String ADDRESS = "http://???.com/";
+
+    public static void createAlert(Context context, String msg){
+
+    }
+}
